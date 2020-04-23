73014b5ae85c70e0d323ec4bcc80be1bca5ab931
Emily <bruner.emilym@gmail.com>
2020-04-23 18:53:32 -0400

    working on day 4 mvp

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -2,6 +2,7 @@ import sys
 sys.path.append('../queue_and_stack')
 from dll_queue import Queue
 from dll_stack import Stack
+from collections import deque
 
 
 class BinarySearchTree:
@@ -12,33 +13,80 @@ class BinarySearchTree:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        if value < self.value:
+            if self.left:
+                self.left.insert(value)
+            else:
+                self.left = BinarySearchTree(value)
+        elif value >= self.value:
+            if self.right:
+                self.right.insert(value)
+            else:
+                self.right = BinarySearchTree(value)
+
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        if target is self.value:
+            return True
+        elif target > self.value:
+            if self.right is None:
+                return False
+            else:
+                return self.right.contains(target)
+            
+        elif target < self.value:
+            if self.left is None:
+                return False
+            else:
+                return self.left.contains(target)
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        if not self.right:
+            return self.value 
+        else:
+            return self.right.get_max()
 
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
     def for_each(self, cb):
-        pass
+        cb(self.value)
+        #apply call back to each node
+        if self.right:
+            self.right.for_each(cb)
+        if self.left:
+            self.left.for_each(cb)
+
 
     # DAY 2 Project -----------------------
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self, node):
-        pass
+        if self.left:
+            self.left.in_order_print(self.left)
+        print(self.value)
+        if self.right:
+            self.right.in_order_print(self.right)
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
     def bft_print(self, node):
-        pass
+        #breadth first so we want a queue aka FIFO
+        q = Queue()
+        #use enqueue instead of append
+
+        while q:
+            print(node.value)
+            if node.left:
+                q.enqueue(node.left)
+            if node.right:
+                q.enqueue(node.right)
+                
+
+
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
---
+++ binary_search_tree/dll_queue.py
@@ -0,0 +1,32 @@
+import sys
+sys.path.append('../doubly_linked_list')
+from doubly_linked_list import DoublyLinkedList
+
+
+
+
+class Queue:
+    def __init__(self):
+        self.size = 0
+        # Why is our DLL a good choice to store our elements?
+        # self.storage = ?
+        self.storage = DoublyLinkedList()
+
+    def enqueue(self, value):
+        #adding item to the back of the queue 
+        # self.size += 1
+        self.storage.add_to_head(value)
+        
+
+    def dequeue(self):
+        #remove and return an item from the front of the queue
+            #if size is 0 return none
+            if self.storage.__len__() is 0:
+                return None
+            else:
+                # self.size -= 1
+                return self.storage.remove_from_tail()
+            
+
+    def len(self):
+        return self.storage.__len__()
